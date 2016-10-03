2f6a309fbb2acc1288b474c3b89c631e14528e03
ohwhos <admin@kimminseok.info>
2016-10-04 02:04:35 +0900

    Update route.php
    사용자 인증을 위한 라우팅

--- application/route/route.php
+++ application/route/route.php
@@ -1,9 +1,11 @@
 <?php
+
 include_once __DIR__."/config.php";
 
-$route->respond("GET", "/auth/login", function($req, $res){
 
-	$res->redirect("/login")->send();
+$route->respond("GET", "/auth/user", function($req, $res, $service, $app){
+	return controller("user")->auth($req, $res);
 });
+
 
 $route->dispatch();
