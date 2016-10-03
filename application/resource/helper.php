b868d399c2d1563e569c08fff3732291de7b85fe
ohwhos <admin@kimminseok.info>
2016-10-04 01:59:41 +0900

    Update helper.php
    라우트에서 사용되는 기본적인 헬프 함수 추가
    - view
    - json
    - layout
    - service
    - controller
    - db

--- application/resource/helper.php
+++ application/resource/helper.php
@@ -6,3 +6,53 @@ $dotenv->load();
 function env($key){
 		return getenv($key);
 }
+
+// db
+function sql($sql,$p=null){
+	return \dcoin\database\Database::sql($sql, $p);
+}
+function fetchAll($sql,$p=null){
+	return \dcoin\database\Database::fetchAll($sql, $p);
+}
+function fetch($sql,$p=null){
+	return \dcoin\database\Database::fetch($sql, $p);
+}
+function rcount($sql,$p=null){
+	return \dcoin\database\Database::count($sql, $p);
+}
+function lastIndex(){
+	return \dcoin\database\Database::lastIndex();
+}
+
+
+// route
+function controller($path){
+	$controller = "";
+
+	$path = explode("/", $path);
+	$path[count($path)-1] = ucfirst($path[count($path)-1])."Controller";
+	$controller = $path[count($path)-1];
+
+	$path = implode("/", $path);
+	require_once(__CNTS__.$path.".php");
+	$class = '\\dcoin\\controllers\\'.$controller;
+	return new $class;
+}
+function service(){
+	global $route;
+	return $route->service();
+};
+
+
+function layout($path, $service){
+	$service->layout(__VIEWS__."/".$path.".php");
+	return $service;
+}
+function view($path){
+	$service = service();
+	$service->render(__VIEWS__."/".$path.".php");
+	return $service;
+}
+function json($data, $pretty=null){
+	return json_encode($data, $pretty ? JSON_PRETTY_PRINT : null);
+}
