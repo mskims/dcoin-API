9d23506f1e66fe6c1ef4276553c5a280317814fa
ohwhos <admin@kimminseok.info>
2016-10-04 02:02:54 +0900

    Update database.php
    네임스페이스 사용을 위한 database.php 파일 수정 및 self 스코프 연산자 추가

--- application/resource/database/database.php
+++ application/resource/database/database.php
@@ -1,11 +1,12 @@
 <?php
+namespace dcoin\database;
 include_once __DIR__."/config.php";
 
 Class Database {
 	private static $db = null;
 	private function init(){
 		global $databaseSet;
-		self::$db = new PDO("mysql:host={$databaseSet['host']};dbname={$databaseSet['dbname']}", $databaseSet["user"], $databaseSet["password"], $databaseSet["pdo_settings"]);
+		self::$db = new \PDO("mysql:host={$databaseSet['host']};dbname={$databaseSet['dbname']}", $databaseSet["user"], $databaseSet["password"], $databaseSet["pdo_setting"]);
 	}
 	public function sql($sql, $p=null){
 		self::init();
@@ -14,13 +15,13 @@ Class Database {
 		return $rs;
 	}
 	public function fetchAll($sql, $p=null){
-		return $this->sql($sql, $p)->fetchAll();
+		return self::sql($sql, $p)->fetchAll();
 	}
-	public function fetch(){
-		return $this->sql($sql, $p)->fetch();
+	public function fetch($sql, $p=null){
+		return self::sql($sql, $p)->fetch();
 	}
-	public function count(){
-		return $this->sql($sql, $p)->rowCount();	
+	public function count($sql, $p=null){
+		return self::sql($sql, $p)->rowCount();	
 	}
 	public function lastIndex(){
 		self::init();
