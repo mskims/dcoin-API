76d12c4f5162bcfb25d3973e2d49e0c63c6a3542
ohwhos <admin@kimminseok.info>
2016-10-04 02:03:48 +0900

    Update index.php
    APP, VIEWS, CNTS(Controllers) define 추가

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 define("__ROOT__", $_SERVER["DOCUMENT_ROOT"]);
+define("__APP__", __ROOT__."/application");
+define("__VIEWS__", __APP__."/views/");
+define("__CNTS__", __APP__."/controllers/");
 
 include_once __ROOT__."/application/server.php";
