14673353bf1236ffaab4cb394932804d5b6e6a8d
ohwhos <admin@kimminseok.info>
2016-10-04 19:42:07 +0900

    Update routing
    로그인 폼 및 API 라우팅 추가

--- application/route/route.php
+++ application/route/route.php
@@ -3,8 +3,25 @@
 include_once __DIR__."/config.php";
 
 
-$route->respond("GET", "/auth/user", function($req, $res, $service, $app){
-	return controller("user")->auth($req, $res);
+$route->respond("GET", "/api/[*]", function($req, $res, $service, $app){
+	return controller("api")->auth($req, $res);
+});
+$route->respond("GET", "/api/user/info", function($req, $res, $service, $app){
+	return controller("user")->getInfo($req, $res);
+});
+$route->respond("GET", "/api/user/name", function($req, $res, $service, $app){
+	return controller("user")->getName($req, $res);
+});
+
+$route->respond("GET", "/api/user/transfer", function($req, $res, $service, $app){
+	return controller("user")->transfer($req, $res);
+});
+
+$route->respond("GET", "/auth/login", function($req, $res, $service, $app){
+	return controller("auth")->login($req, $res, $service);
+});
+$route->respond("POST", "/auth/login_process", function($req, $res, $service, $app){
+	return controller("auth")->loginProcess($req, $res, $service);
 });
 
 
