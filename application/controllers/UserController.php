cfa1ff32eb058c9714866cbd5b3c5c40e9e8de07
ohwhos <admin@kimminseok.info>
2016-10-04 19:45:44 +0900

    Update UserController
    - 허용컬럼 추가
    - account_number으로 사용자 이름을 가져오는 메소드 추가
    - transfer 메소드 생성

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -2,9 +2,24 @@
 namespace dcoin\controllers;
 
 Class UserController {
-	public function auth($req, $res){
-		$req->pw = md5($req->pw);
-		$data = fetchAll("SELECT * FROM member WHERE account_number=? AND pw=?", [$req->id, $req->pw]);
-		return json($data[0], true);
+	private $allowedColumns = ["name", "account_number", "thumb", "money"];
+	public function getInfo($req, $res){
+		$req->items = explode("|", $req->items);
+		$columns = ["name", "account_number", "money"];
+		foreach($req->items as $column){
+			if(true === in_array($column, $this->allowedColumns)){
+				$columns[] = $column;
+			}
+		}
+		$columns = implode(", ", array_unique($columns));
+		$data = fetch("SELECT {$columns} FROM member WHERE skey=?", [$req->key]);
+		return json($data);
+	}
+	public function getName($req, $res){
+		$data = fetch("SELECT name FROM member WHERE account_number=?", [$req->account_number]);
+		return json($data);
+	}
+	public function transfer($req, $res){
+		// @todo
 	}
 }
