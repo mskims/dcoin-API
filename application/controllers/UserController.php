0bdae40e39171b523abb7ae5b82262b1e1e1dc7d
ohwhos <admin@kimminseok.info>
2016-10-04 15:34:05 +0900

    Update UserController.php
    auth 메소드가 결과 배열의 첫번째만 반환하도록 변경

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -5,6 +5,6 @@ Class UserController {
 	public function auth($req, $res){
 		$req->pw = md5($req->pw);
 		$data = fetchAll("SELECT * FROM member WHERE account_number=? AND pw=?", [$req->id, $req->pw]);
-		return json($data, true);
+		return json($data[0], true);
 	}
 }
