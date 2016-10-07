3ae7dc5eb9ac8ac9f0b3568dc0c13318327769cf
ohwhos <admin@kimminseok.info>
2016-10-07 11:03:48 +0900

    Update login view
    앱 이름 설명 잋 버튼 클래스 변경

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -2,6 +2,9 @@
 	<input type="hidden" name="csrf_token" value="<?=$this->csrf_token?>">
 	<input type="hidden" name="redirect_url" value="<?=$this->redirect_url?>">
 	<div class="form-group">
+		디지텍코인 로그인으로 <strong><?=$this->app_info["name"]?></strong> 서비스를 이용하실 수 있습니다.
+	</div>
+	<div class="form-group">
 		<input type="text" name="account_number" placeholder="아이디" class="form-control" required="">
 	</div>
 	<div class="form-group">
@@ -12,5 +15,5 @@
 		 <?php foreach($this->errors as $error){ echo $error."<br>"; } ?>
 	</div>
 	<?php } ?>
-	<button class="btn btn-success">로그인</button>
+	<button class="btn btn-info">로그인</button>
 </form>
