3086c8df76e185ee5ae0a1ced6d4bace8653b7ad
ohwhos <admin@kimminseok.info>
2016-10-07 10:53:04 +0900

    Update AppController
    app_idx의 스코프들을 배열로 리턴하는 getScopes 메소드 추가

--- application/controllers/AppController.php
+++ application/controllers/AppController.php
@@ -6,6 +6,9 @@ Class AppController {
 		$columns = implode(",", $columns);
 		return fetch("SELECT {$columns} FROM apps WHERE idx=?", [$app_idx]);
 	}
+	public function getScopes($app_idx){
+		return explode("|", fetch("SELECT GROUP_CONCAT(scope SEPARATOR '|') AS scopes FROM scopes WHERE app_idx=?", [$app_idx])["scopes"]);
+	}
 	
 	
 }
