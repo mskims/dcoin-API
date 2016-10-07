6b490e1898d61d43fc676007987e94d14fa756e4
ohwhos <admin@kimminseok.info>
2016-10-07 10:57:06 +0900

    Update routing
    oauto 구조를 위한 기본 라우팅

--- application/route/route.php
+++ application/route/route.php
@@ -6,8 +6,8 @@ include_once __DIR__."/config.php";
 $route->respond("GET", "/api/[*]", function($req, $res, $service, $app){
 	return controller("api")->auth($req, $res);
 });
-$route->respond("GET", "/api/user/info", function($req, $res, $service, $app){
-	return controller("user")->getInfo($req, $res);
+$route->respond("GET", "/api/me/info", function($req, $res, $service, $app){
+	return controller("user")->getPublicInfo($req, $res);
 });
 $route->respond("GET", "/api/user/name", function($req, $res, $service, $app){
 	return controller("user")->getName($req, $res);
@@ -17,12 +17,16 @@ $route->respond("GET", "/api/user/transfer", function($req, $res, $service, $app
 	return controller("user")->transfer($req, $res);
 });
 
+
+// AUTH
 $route->respond("GET", "/auth/login", function($req, $res, $service, $app){
 	return controller("auth")->login($req, $res, $service);
 });
 $route->respond("POST", "/auth/login_process", function($req, $res, $service, $app){
 	return controller("auth")->loginProcess($req, $res, $service);
 });
-
+$route->respond("/auth/access_token", function($req, $res, $service,$app){
+	return controller("auth")->accessToken($req);
+});
 
 $route->dispatch();
