0bc4471aff0f42e6f9ca45098b0e6d6e734b82da
ohwhos <admin@kimminseok.info>
2016-10-07 10:59:50 +0900

    Add logo
    사이즈별 로고파일 추가와 이미지태그 삽입

--- application/views/layouts/main.layout.php
+++ application/views/layouts/main.layout.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <style>
         .wrap { width: 500px; margin: 100px auto; }
+        img { max-width: 100%; vertical-align: middle; }
         h1 { text-align: center; margin-bottom: 20px; }
         button { width: 100%; }
         .errors { font-weight: bold; color: #f00; }
@@ -13,7 +14,7 @@
 </head>
 <body>
 <div class="wrap">
-    <header><h1>디지텍코인</h1></header>
+    <header><h1><img src="http://static1.textcraft.net/data1/1/7/1783f0e02a4687c2c2b3fdf06f475aef13af86c0da39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd80709cb107d2d0c48f712b731fbfcb8f3a93d.png" alt=""></h1></header>
     <div class="view">
         <?=$this->yieldView()?>    
     </div>
